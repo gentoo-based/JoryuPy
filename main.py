623942133369014e339bbe43aaf0315ea4edea23
Jake <Anonimized Profile 1>
2025-05-23 09:11:58 +0800

    Push from mobile terminal

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+from os import getenv
 from joryu import JoryuPy, get_prefix
 from dotenv import load_dotenv
 from discord import Intents
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = getenv("DISCORD_TOKEN")
 
 JoryuPy(intents=Intents.all(), command_prefix=get_prefix).run(TOKEN)
